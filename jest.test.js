b3026d4ab9551dc49ff939a50c8be0d4eebda8e2
安风 <jiadongyao@cai-inc.com>
2023-03-29 17:53:19 +0800

    feat

--- jest.test.js
+++ jest.test.js
@@ -1,12 +1,3 @@
-/*
- * @Author: 安风 jiadongyao@cai-inc.com
- * @Date: 2023-02-23 22:11:23
- * @LastEditors: 安风 jiadongyao@cai-inc.com
- * @LastEditTime: 2023-02-26 14:24:48
- * @FilePath: /auto-test-jest-learn/jest.test.js
- * @Description:
- */
-
 // 因为已经配置了babel，所以可以使用 es6 的方式进行引入
 import { ZcyZooTeam, happyDay } from './index';
 
